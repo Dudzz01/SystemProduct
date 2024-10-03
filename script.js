1352fa34c17b00db30a986cdeb2b430f65023a71
Eduardo Mury Assafin <101987520+Dudzz01@users.noreply.github.com>
2024-10-03 09:32:06 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,32 +1,115 @@
 document.addEventListener("DOMContentLoaded", function(ev)
 {
     ev.preventDefault();
-    document.querySelector("#form-products").addEventListener("click", ev => 
+const openPopupBtn = document.getElementById('openPopupBtn');
+const popupForm = document.getElementById('popupForm');
+const closeBtn = document.querySelector('.close');
+const formPagina = document.querySelector('#form-products');
+const elementFormPopUp = document.querySelector('#popupForm-id');
+let produtos = [];
+
+
+
+
+    document.querySelector("#submitButton").addEventListener("click", ev => 
     {
         ev.preventDefault();
-        let lista = document.querySelector("#lista-produtos");
-
-        let newItem = document.createElement("li");
-        let newItem2 = document.createElement("li");
-
-        let nomeProduto = document.getElementById("product-name").value;
-        let valorProduto = document.getElementById("product-value").value;
-
-        newItem.innerText = nomeProduto;
-        newItem2.innerText = valorProduto;
-
-        newItem.classList.add("itemNameProduct");
-        newItem2.classList.add("itemValorProduct");
-
-        if(lista.childElementCount <= 0)
+        if(formPagina.checkValidity() )
         {
+            
+           
+            let nomeProduto = document.getElementById("product-name").value;
+            let valorProduto = parseFloat(document.getElementById("product-value").value); 
+            if (nomeProduto && !isNaN(valorProduto)) {
+                
+                produtos.push({ nome: nomeProduto, valor: valorProduto });
 
-            lista.appendChild(newItem);
-            lista2.appendChild(newItem);
+                
+                ordenarEAtualizarLista();
+            } 
+        
         }
-
+        else
+        {
+            if(!formPagina.checkValidity())
+            {
+                formPagina.reportValidity();
+            }
+        }
         
     }
     )
+
+    document.querySelector("#submitButtonPopup").addEventListener("click", function(ev) {
+        ev.preventDefault(); 
+
+        document.getElementById("form-products").reset();
+
+        if(formPagina.checkValidity() || elementFormPopUp.checkValidity())
+        {
+            let nomeProduto = document.getElementById("popup-product-name").value;
+            let valorProduto = parseFloat(document.getElementById("popup-product-value").value); 
+          
+            produtos.push({ nome: nomeProduto, valor: valorProduto });
+
+         
+            ordenarEAtualizarLista();
+
+            elementFormPopUp.reset();
+            popupForm.style.display = 'none'; 
+        } else {
+            elementFormPopUp.reportValidity();
+        }
+    });
+
+    function ordenarEAtualizarLista() {
+        
+        produtos.sort((a, b) => a.valor - b.valor);
+    
+       
+        let listaNomeProduto = document.querySelector("#lista-nome-id");
+        let listaValorProduto = document.querySelector("#lista-valor-id");
+        listaNomeProduto.innerHTML = '';
+        listaValorProduto.innerHTML = '';
+        
+        produtos.forEach(produto => {
+            let newItemNome = document.createElement("li");
+            let newItem2Valor = document.createElement("li");
+    
+            newItemNome.innerText = produto.nome;
+            newItem2Valor.innerText = produto.valor;
+            console.log(produto.nome);
+            console.log(produto.valor)
+    
+            newItemNome.classList.add("itemNameProduct");
+            newItem2Valor.classList.add("itemValorProduct");
+    
+            listaNomeProduto.appendChild(newItemNome);
+            listaValorProduto.appendChild(newItem2Valor);
+        });
+    }
+
+
+ 
+
+
+openPopupBtn.addEventListener('click', function() {
+    document.getElementById("form-products").reset();
+    popupForm.style.display = 'flex';
+});
+
+
+closeBtn.addEventListener('click', function() {
+    popupForm.style.display = 'none';
+    elementFormPopUp.reset();
+});
+
+
+window.addEventListener('click', function(event) {
+    if (event.target == popupForm) {
+        popupForm.style.display = 'none';
+        elementFormPopUp.reset();
+    }
+});
 }
 )
